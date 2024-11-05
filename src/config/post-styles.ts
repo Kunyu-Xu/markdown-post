b8732781b572daa6d06083ce0336eed2ee37136a
yuki <108104558+Kunyu-Xu@users.noreply.github.com>
2024-11-05 17:41:56 +0800

    Added new note styles to better support multi-level content reading (#1)
    * Added new note styles to better support multi-level content reading
    
    * feat en translation

--- src/config/post-styles.ts
+++ src/config/post-styles.ts
@@ -2,12 +2,14 @@ import githubStyle from "@/styles/github.css?raw";
 import newspaperStyle from "@/styles/newspaper.css?raw";
 import posterStyle from "@/styles/poster.css?raw";
 import slimStyle from "@/styles/slim.css?raw";
+import noteStyle from "@/styles/note.css?raw";
 
 export const markdownStyles = [
   { name: "github", css: githubStyle },
   { name: "newspaper", css: newspaperStyle },
   { name: "poster", css: posterStyle },
   { name: "slim", css: slimStyle },
+  { name: "note", css: noteStyle },
 ];
 
 export const loadCSS: any = (name: string) =>
